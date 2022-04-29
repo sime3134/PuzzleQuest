e2d8b39c51f3a43835ce71b8657ec7ad65fb24b2
JSalomonsson <johansalomonsson1@gmail.com>
2022-04-29 09:56:51 +0200

    progress with the maze

--- src/main/state/GameState.java
+++ src/main/state/GameState.java
@@ -34,7 +34,7 @@ public class GameState extends State{
     private boolean repeatMaps;
 
     private String[][] worldMap = {
-            {"map1", "map2", "map3", "map4", "map5"},
+            {"maze", "map2", "map3", "map4", "map5"},
             {"map6", "map7", "map8", "map9", "map10"},
             {"map11", "map12", "map13", "map14", "map15"},
             {"map16", "map17", "map18", "map19", "map20"},
@@ -174,7 +174,7 @@ public class GameState extends State{
     private void initializeEntities(Game game) {
         player = new Player(PlayerController.getInstance(),
                 game.getContent().getSpriteSet("player"));
-        player.setPosition(new Vector2D(1300,1000));
+        player.setPosition(new Vector2D(200,200));
 
         game.getGameObjects().add(player);
         game.getCamera().focusOn(player);
